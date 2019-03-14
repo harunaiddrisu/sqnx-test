3274cffc6692c1abfd77afb3c6488eee722950f9
Haruna Iddrisu <iddrisu92@gmail.com>
2019-03-14 11:55:28 +0100

    adds prod. build

---
+++ build/precache-manifest.477a2411e83c3f5d2b4f936c91d6142f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1fda65a2c6eb8d1c6e6c",
+    "url": "/static/js/main.91af25c2.chunk.js"
+  },
+  {
+    "revision": "d4157ec5f9f7d1029046",
+    "url": "/static/js/2.2f1e7cf2.chunk.js"
+  },
+  {
+    "revision": "1fda65a2c6eb8d1c6e6c",
+    "url": "/static/css/main.94c3c97d.chunk.css"
+  },
+  {
+    "revision": "16a2caf1326349ea2c57857336056f00",
+    "url": "/index.html"
+  }
+];
